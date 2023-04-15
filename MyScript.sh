0872add9d53b8c5f59998095e53e788c2401d4e2
grstfv <grstfv@bk.ru>
2023-04-15 13:51:15 +0400

    add_Bash_script

---
+++ MyScript.sh
@@ -0,0 +1,9 @@
+#1/bin/bash
+
+cd MainFolder
+mkdir Folder7 Folder8 Folder9
+cd Folder9
+touch readme4.txt readme5.txt readme6.txt data3.json data4.json
+mkdir Folder10 Folder11 Folder12
+ls -la
+mv readme?.txt Folder10
